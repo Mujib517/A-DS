5f7be0c899820f368dc61152588bff8972821588
Rahman <mr185242@ncr.com>
2017-10-16 13:16:54 +0530

    First Commit'

---
+++ AandDS/Arrays/Queries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AandDS.Arrays
+{
+    public class Queries
+    {
+        public int[] Source { get; set; } = new[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        public Queries()
+        {
+            //sum : [10,30,60,100,150,210,280,360,450,550]
+            for (int i = 1; i < Source.Length; i++)
+            {
+                Source[i] += Source[i - 1];
+            }
+        }
+
+
+
+        public int GetSum(int start, int end)
+        {
+            return 0;
+        }
+
+
+    }
+}
---
+++ AandDS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AandDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
