24ae33c859772691d3d7e88ce6231bab6a9b60e8
Rahman <mr185242@ncr.com>
2017-10-30 12:23:56 +0530

    Sqrt

---
+++ AandDS/Demo/Sqrt.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AandDS.Demo
+{
+    public class Sqrt
+    {
+        //O(N)
+        public static int Solution1(int n)
+        {
+            for (var i = 0; i <= n; i++)
+            {
+                if (i * i == n) return i;
+            }
+            return 0;
+        }
+
+        //O(N)
+        public static int Solution2(int n)
+        {
+            if (n == 1) return 1;
+            for (var i = 0; i <= n / 2; i++)
+            {
+                if (i * i == n) return i;
+            }
+
+            return 0;
+        }
+
+        //O(LogN) binary search
+        public static int Solution3(int n)
+        {
+            if (n == 1) return 1;
+
+            int start = 1;
+            int end = n;
+            int mid = (start + end) / 2;
+
+            while (start < end)
+            {
+                var sum = mid * mid;
+                if (sum == n) return mid;
+                else if (sum > n) end = mid;
+                else start = mid;
+
+                mid = (start + end) / 2;
+            }
+
+            return 0;
+        }
+    }
+}
--- AandDS/Program.cs
+++ AandDS/Program.cs
@@ -7,7 +7,17 @@ namespace AandDS
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine(Sqrt.Solution3(0));
+            Console.WriteLine(Sqrt.Solution3(1));
+            Console.WriteLine(Sqrt.Solution3(4));
+            Console.WriteLine(Sqrt.Solution3(9));
+            Console.WriteLine(Sqrt.Solution3(16));
+            Console.WriteLine(Sqrt.Solution3(25));
+            Console.WriteLine(Sqrt.Solution3(36));
+            Console.WriteLine(Sqrt.Solution3(49));
+            Console.WriteLine(Sqrt.Solution3(64));
+            Console.WriteLine(Sqrt.Solution3(81));
+            Console.WriteLine(Sqrt.Solution3(100));
         }
     }
 }
