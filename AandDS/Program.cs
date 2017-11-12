a4f5f48bde4eb877a46bc9a64a4ed771cc20fada
Mujib517 <mujib517@gmail.com>
2017-11-12 21:04:11 +0530

    Assignments

--- AandDS/Program.cs
+++ AandDS/Program.cs
@@ -8,7 +8,31 @@ namespace AandDS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Week1.Random.Solution1(3, 5)); //0 1 0 1
+
+            var str = "1 2 7 9 5 6 3 8";
+
+            int[] arr = Array.ConvertAll(str.Split(' '), Int32.Parse);
+            //var input3 = new[] { 8, 11, 10, 2, 7, 4, 3, 5, 1, 6 };
+            //var input2 = new[] { 3, 5, 8, 1, 4, 7, 2 };
+            //var input = new[] { 1, 2, 7, 9, 5, 6, 3, 8 };
+
+            //var str = "1 2 3 4";
+
+            //Array.ConvertAll(str.Split(' '), int.Parse);
+
+            //Console.WriteLine(Assignments.FindMissingNumber(input));
+            //Console.WriteLine(Assignments.FindMissingNumber(input2));
+            //Console.WriteLine(Assignments.FindMissingNumber(input3));
+
+
+            //int testCases = Convert.ToInt32(Console.ReadLine());
+            //while (testCases > 0)
+            //{
+            //    int size = Convert.ToInt32(Console.ReadLine());
+            //    int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            //    Console.WriteLine(FindMissingNumber(arr, size));
+            //    testCases--;
+            //}
         }
     }
 }
---
+++ AandDS/Week1/Assignments.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AandDS.Week1
+{
+    public class Assignments
+    {
+        //timeout error.
+        public static void ToBinary(double n)
+        {
+            int i;
+            int[] a = new int[65];
+            for (i = 0; n > 0; i++)
+            {
+                a[i] = (int)n % 2;
+                n = n / 2;
+            }
+            for (i = i - 1; i >= 0; i--)
+                Console.Write(a[i]);
+            Console.WriteLine();
+        }
+
+        public static bool IsPowerOf2(long n)
+        {
+            var count = 0;
+            for (var i = 0; i < 64; i++)
+            {
+                if (((n >> i) & 1) > 0) count++;
+                if (count > 1) return false;
+            }
+            return true;
+        }
+
+        public static int FindMissingNumber(int[] arr)
+        {
+            int actualSum = 0;
+            //O(N)
+            for (int i = 0; i < arr.Length; i++)
+            {
+                actualSum += arr[i];
+            }
+
+            int n = arr.Length + 1;
+            //Sum of natural numbers formual n(n+1)/2
+            int expectedSum = (n * (n + 1)) / 2;
+            return expectedSum - actualSum;
+        }
+    }
+}
