65c101d6f1ecdd31cbe54698f8ed199b09357b67
Mujib517 <mujib517@gmail.com>
2017-11-19 22:54:36 +0530

    Week2

--- AandDS/Program.cs
+++ AandDS/Program.cs
@@ -1,6 +1,7 @@
 ﻿using AandDS.Demo;
 using AandDS.Week1;
 using System;
+using System.Collections.Generic;
 
 namespace AandDS
 {
@@ -8,10 +9,14 @@ namespace AandDS
     {
         static void Main(string[] args)
         {
-            var n = 5;
 
-            Console.WriteLine(Convert.ToString(0x5555, 2));
-            Console.WriteLine(n & 0xaa);
+            var a = new[] {3,7,10 };
+            var b = new[] { 5,20,15 };
+
+
+            Console.WriteLine(Week2.Assignments.GetAWinningChances(a, b, 2));
+
+
 
             //Console.WriteLine(Assignments.SwapBits(10));
             //Console.WriteLine(Assignments.SwapBits(7));
@@ -23,7 +28,7 @@ namespace AandDS
             //while (testCases > 0)
             //{
             //    int size = Convert.ToInt32(Console.ReadLine());
-            //    int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            //    int[] arr = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), int.Parse);
             //    Console.WriteLine(FindMissingNumber(arr, size));
             //    testCases--;
             //}
---
+++ AandDS/Week2/Assignments.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AandDS.Week2
+{
+    public class Assignments
+    {
+        //Tower of hanoi
+        public static void TOH(int n, char src, char dest, char temp, List<String> result)
+        {
+            if (n == 0) return;
+            TOH(n - 1, src, temp, dest, result);
+            result.Add("Move " + n + " from " + src + " to " + dest);
+            TOH(n - 1, temp, dest, src, result);
+        }
+
+        //Find out two repeated numbers in an array. array contains 
+        //n+2 elements raning from 1 to n
+        public static void Repeated2Numbers(int[] arr)
+        {
+            Array.Sort(arr);
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1]) Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        //Every number repeats thrice in an array except one.
+        public static int NonRepeatingNumber(int[] arr)
+        {
+            Dictionary<int, int> count = new Dictionary<int, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (count.ContainsKey(arr[i])) count[arr[i]]++;
+                else count.Add(arr[i], 1);
+            }
+
+            foreach (var key in count.Keys)
+            {
+                if (count[key] == 1) return key;
+            }
+            return -1;
+        }
+
+        //find out whether a[i]+a[j]=k element exists or not where i<j
+        public static bool SumOfParis(int[] arr, int n, int k)
+        {
+            Array.Sort(arr);
+            int p1 = 0;
+            int p2 = n - 1;
+
+            while (p1 < p2)
+            {
+                int sum = arr[p1] + arr[p2];
+                if (sum < k) p1++;
+                else if (sum > k) p2--;
+                else return true;
+            }
+
+            return false;
+        }
+
+        public static void BubbleSort(int[] arr, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        Swap(ref arr[i], ref arr[j]);
+                        Console.Write(j + " ");
+                    }
+                }
+            }
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static string LargestNumberConcatenated(string[] arr)
+        {
+            Array.Sort(arr, new NumberComparer());
+            return string.Join("", arr);
+        }
+
+        public static int MaxContSubSeq(int[] a, int size)
+        {
+            int[] arr = a.Distinct().ToArray();
+            size = arr.Length;
+            int[] match = new int[size];
+            int max = 0;
+
+            for (var i = 0; i < size; i++)
+            {
+                for (var j = i + 1; j < size; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+
+                        match[i]++;
+                        if (match[i] > max) max = match[i];
+
+                    }
+                }
+            }
+
+            return max;
+        }
+
+        public static int GetAWinningChances(int[] teamA, int[] teamB, int size)
+        {
+            int count = 0;
+
+            //Array.Sort(teamA);
+            //Array.Sort(teamB);
+
+            int p1 = size - 1;
+            int p2 = size - 1;
+
+            while (p1 >= 0 && p2 >= 0)
+            {
+                if (teamA[p1] > teamB[p2])
+                {
+                    count++;
+                    p1--;
+                }
+                p2--;
+            }
+
+            return count;
+        }
+    }
+
+    public class NumberComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return (y + x).CompareTo((x + y));
+        }
+    }
+}
