72f85526e8ffafa428f015231ea97d82e2f21b7d
Mujib517 <mujib517@gmail.com>
2017-12-03 18:49:10 +0530

    Firsst Contents

--- AandDS/Program.cs
+++ AandDS/Program.cs
@@ -13,17 +13,17 @@ namespace AandDS
         {
             int[] arr = new[] { 8, 5, 10, 3, 6, 1, 2, 4, 8, 14 };
 
-            Console.WriteLine(Week3.Assignments.Calculate(arr, arr.Length, 3));
+            Console.WriteLine(Week3.Assignments.CountDivisors(9));
 
 
-            //int testCases = Convert.ToInt32(Console.ReadLine());
-            //while (testCases > 0)
-            //{
-            //    int size = Convert.ToInt32(Console.ReadLine());
-            //    int[] arr = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), int.Parse);
-            //    Console.WriteLine(FindMissingNumber(arr, size));
-            //    testCases--;
-            //}
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            while (testCases > 0)
+            {
+                int size = Convert.ToInt32(Console.ReadLine());
+                int[] arr = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), int.Parse);
+                Console.WriteLine(FindMissingNumber(arr, size));
+                testCases--;
+            }
         }
     }
 }
--- AandDS/Week3/Assignments.cs
+++ AandDS/Week3/Assignments.cs
@@ -194,10 +194,42 @@ namespace AandDS.Week3
             return count == k - 1;
         }
 
+
+        public static string IsPanlindrom(string str, int n)
+        {
+            int p1 = 0;
+            int p2 = n - 1;
+            while (p1 < p2)
+            {
+                if (str[p1] != str[p2]) return "No";
+                p1++;
+                p2--;
+            }
+            return "Yes";
+        }
+
+        public static int CountDivisors(double n)
+        {
+            int count = 0;
+            for (int i = 1; i <= Math.Sqrt(n) + 1; i++)
+            {
+                if (n % i == 0)
+                {
+                    if (n / i == i)
+                        count++;
+                    else count += 2;
+                }
+            }
+            return count;
+        }
+
         class Data
         {
             public int Key { get; set; }
             public int Frequency { get; set; }
+
+
+
         }
 
         class DataComparer : IComparer<Data>
