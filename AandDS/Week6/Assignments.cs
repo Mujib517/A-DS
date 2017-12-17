f4c3d99c84d0528c5b3c6ec6b50ddf28e7842479
Mujib517 <mujib517@gmail.com>
2017-12-17 23:03:52 +0530

    Week 6

---
+++ AandDS/Week6/Assignments.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AandDS.Week6
+{
+    class Assignments
+    {
+        static void Reverse(string[] arr)
+        {
+            Stack<string> stack = new Stack<string>();
+            foreach (var word in arr)
+            {
+                stack.Push(word);
+            }
+
+            while (stack.Count != 0) Console.Write(stack.Pop() + " ");
+            Console.WriteLine();
+        }
+    }
+
+    class CustomStack
+    {
+
+        private int[] arr = new int[201];
+        private int count = -1;
+
+        public void Push(int x)
+        {
+            arr[++count] = x;
+        }
+
+        public void Pop()
+        {
+            if (count == -1) Console.WriteLine("Empty");
+            else Console.WriteLine(arr[count--]);
+        }
+    }
+}
